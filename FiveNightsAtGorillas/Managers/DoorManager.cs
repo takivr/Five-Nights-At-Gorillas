58b47478b8e7c7b9d7382898128890d684641c80
MrBanana <logande4610@gmail.com>
2023-11-21 15:11:14 -0600

    hopefully finished the door manager

--- FiveNightsAtGorillas/Managers/DoorManager.cs
+++ FiveNightsAtGorillas/Managers/DoorManager.cs
@@ -8,10 +8,133 @@ using FiveNightsAtGorillas.Managers.NetworkedData;
 
 namespace FiveNightsAtGorillas.Managers.DoorAndLight
 {
-    public class DoorManager : MonoBehaviourPun
+    public class DoorManager : MonoBehaviourPunCallbacks, IInRoomCallbacks
     {
         public static DoorManager Data;
+        public int ButtonTimerDelay { get; private set; } = 3;
+        public bool CanUseLeftButton { get; private set; } = true;
+        public bool CanUseRightButton { get; private set; } = true;
+        public bool RightDoorOpen { get; private set; } = true;
+        public bool LeftDoorOpen { get; private set; } = true;
 
         void Awake() { Data = this; }
+
+        public void UseLocalDoor(bool isRight)
+        {
+            if(isRight)
+            {
+                if (RightDoorOpen)
+                {
+                    CloseOpenDoor(true, true);
+                }
+                else
+                {
+                    CloseOpenDoor(true, false);
+                }
+            }
+            else
+            {
+                if(LeftDoorOpen)
+                {
+                    CloseOpenDoor(false, true);
+                }
+                else
+                {
+                    CloseOpenDoor(false, false);
+                }
+            }
+        }
+
+        public void UseOnlineDoor(bool isRight)
+        {
+            if (isRight)
+            {
+                if (RightDoorOpen)
+                {
+                    object[] value = new object[] { PhotonData.Key.RightDoor, PhotonData.Key.Close };
+                    RaiseEventOptions options = new RaiseEventOptions() { CachingOption = EventCaching.DoNotCache, Receivers = ReceiverGroup.Others };
+                    PhotonNetwork.RaiseEvent((byte)PhotonData.Key.RightDoor, value, options, SendOptions.SendReliable);
+                }
+                else
+                {
+                    object[] value = new object[] { PhotonData.Key.RightDoor, PhotonData.Key.Open };
+                    RaiseEventOptions options = new RaiseEventOptions() { CachingOption = EventCaching.DoNotCache, Receivers = ReceiverGroup.Others };
+                    PhotonNetwork.RaiseEvent((byte)PhotonData.Key.RightDoor, value, options, SendOptions.SendReliable);
+                }
+            }
+            else
+            {
+                if (LeftDoorOpen)
+                {
+                    object[] value = new object[] { PhotonData.Key.LeftDoor, PhotonData.Key.Close };
+                    RaiseEventOptions options = new RaiseEventOptions() { CachingOption = EventCaching.DoNotCache, Receivers = ReceiverGroup.Others };
+                    PhotonNetwork.RaiseEvent((byte)PhotonData.Key.LeftDoor, value, options, SendOptions.SendReliable);
+                }
+                else
+                {
+                    object[] value = new object[] { PhotonData.Key.LeftDoor, PhotonData.Key.Open };
+                    RaiseEventOptions options = new RaiseEventOptions() { CachingOption = EventCaching.DoNotCache, Receivers = ReceiverGroup.Others };
+                    PhotonNetwork.RaiseEvent((byte)PhotonData.Key.LeftDoor, value, options, SendOptions.SendReliable);
+                }
+            }
+        }
+
+        public void OnEvent(EventData photonEvent)
+        {
+            object[] receivedData = (object[])photonEvent.CustomData;
+            string Action = (string)receivedData[1];
+            switch (photonEvent.Code)
+            {
+                case (byte)PhotonData.Key.RightDoor:
+                    if (Action == PhotonData.Key.Close.ToString()) { CloseOpenDoor(true, true); }
+                    if (Action == PhotonData.Key.Open.ToString()) { CloseOpenDoor(true, true); }
+                    break;
+                case (byte)PhotonData.Key.LeftDoor:
+                    if (Action == PhotonData.Key.Close.ToString()) { CloseOpenDoor(false, true); }
+                    if (Action == PhotonData.Key.Open.ToString()) { CloseOpenDoor(false, true); }
+                    break;
+            }
+        }
+
+        void CloseOpenDoor(bool isRight, bool isClose)
+        {
+            if(isRight)
+            {
+                if (isClose)
+                {
+                    RefrenceManager.Data.RightDoorAnimation.Play("Right Door Close");
+                    RightDoorOpen = false;
+                    StartCoroutine(ButtonDelay(true));
+                }
+                else
+                {
+                    RefrenceManager.Data.RightDoorAnimation.Play("Right Door OPen");
+                    RightDoorOpen = true;
+                    StartCoroutine(ButtonDelay(true));
+                }
+            }
+            else
+            {
+                if (isClose)
+                {
+                    RefrenceManager.Data.LeftDoorAnimation.Play("Left Door Close");
+                    LeftDoorOpen = false;
+                    StartCoroutine(ButtonDelay(false));
+                }
+                else
+                {
+                    RefrenceManager.Data.LeftDoorAnimation.Play("Left Door Open");
+                    LeftDoorOpen = true;
+                    StartCoroutine(ButtonDelay(false));
+                }
+            }
+        }
+
+        IEnumerator ButtonDelay(bool isRight)
+        {
+            if (isRight) { CanUseRightButton = false; } else { CanUseLeftButton = false; }
+            yield return new WaitForSeconds(ButtonTimerDelay);
+            if (isRight) { CanUseRightButton = true; } else { CanUseLeftButton = true; }
+        }
     }
 }
--- FiveNightsAtGorillas/Managers/PhotonData.cs
+++ FiveNightsAtGorillas/Managers/PhotonData.cs
@@ -1,26 +1,26 @@
-﻿using Photon.Pun;
+﻿using ExitGames.Client.Photon;
+using FiveNightsAtGorillas.Managers.DoorAndLight;
+using Photon.Pun;
 using Photon.Realtime;
 
 namespace FiveNightsAtGorillas.Managers.NetworkedData
 {
     public class PhotonData : MonoBehaviourPunCallbacks
     {
-        public enum RaiseEventValues
+        public enum Key
         {
-            ChangeDoorState = 0,
-            ChangeLightState = 1,
-            SwitchCamera = 2,
-            ChangeGorillaState = 3,
-            ChangeBobState = 4,
-            ChangeMingusState = 5,
-            ChangeDingusState = 6
+            RightDoor = 0,
+            LeftDoor = 1,
+            GorillaCam = 3,
+            BobCam = 4,
+            Mingus = 5,
+            Dingus = 6,
+            Close = 7,
+            Open = 8
         }
 
         public static PhotonData instance;
-        public Player CurrentMasterClient;
-        public ExitGames.Client.Photon.Hashtable RoomProperties = new ExitGames.Client.Photon.Hashtable();
 
         void Awake() { instance = this; }
-        public override void OnMasterClientSwitched(Player newMasterClient) { CurrentMasterClient = newMasterClient; }
     }
 }
