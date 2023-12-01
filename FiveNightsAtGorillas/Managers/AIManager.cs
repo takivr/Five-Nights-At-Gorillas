92ecee21792d8a41ee2f417fd94fe2e355fbe118
MrBanana <logande4610@gmail.com>
2023-12-01 16:17:45 -0600

    made it so if your cam is on cam3 while gorilla is there, he can't move

--- FiveNightsAtGorillas/Managers/AIManager.cs
+++ FiveNightsAtGorillas/Managers/AIManager.cs
@@ -84,7 +84,7 @@ namespace FiveNightsAtGorillas.Managers.AI
         #region EnemyMove
         void GorillaLocalDelay()
         {
-            if (CamPos == "Cam3") { if (DoorManager.Data.RightDoorOpen) { FNAG.Data.Jumpscare(); } else { MoveGorilla("Cam10"); } }
+            if (CamPos == "Cam3") { if (DoorManager.Data.RightDoorOpen) { if (CameraManager.Data.CurrentCameraPos != "Cam3") { FNAG.Data.Jumpscare(); } } else { MoveGorilla("Cam10"); } }
             else if (CamPos == "Cam4") { int random = Random.Range(1, 3); if (random == 1) { MoveGorilla("Cam3"); } else { MoveGorilla("Cam10"); } }
             else if (CamPos == "Cam10") { int random = Random.Range(1, 3); if (random == 1) { MoveGorilla("Cam5"); } else { MoveGorilla("Cam4"); } }
             else if (CamPos == "Cam5") { MoveGorilla("Cam10"); }
