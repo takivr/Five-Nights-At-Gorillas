3415376ae66ec9a7d55eee9aa82aeb852163af68
MrBanana <logande4610@gmail.com>
2023-12-02 13:02:56 -0600

    Fixed a menu issue

--- FiveNightsAtGorillas/FNAG.cs
+++ FiveNightsAtGorillas/FNAG.cs
@@ -216,9 +216,6 @@ namespace FiveNightsAtGorillas
             InCustomRoom = false;
             StopGame();
             RefrenceManager.Data.Menu.SetActive(false);
-            RefrenceManager.Data.MenuScrollLeft.SetActive(false);
-            RefrenceManager.Data.MenuScrollRight.SetActive(false);
-            RefrenceManager.Data.MenuWarning.SetActive(true);
             GameObject.Find("Selects").SetActive(false);
         }
 
